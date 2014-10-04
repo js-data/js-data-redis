fa63c48e8a8d5567c6cb214146b153caa6d695ad
Jason Dobry <jason.dobry@gmail.com>
2014-10-03 22:55:52 -0600

    Initial Release.

--- README.md
+++ README.md
@@ -1,4 +1,80 @@
-js-data-redis
-=============
+<img src="https://raw.githubusercontent.com/js-data/js-data/master/js-data.png" alt="js-data logo" title="js-data" align="right" width="64" height="64" />
 
-Redis adapter for js-data
+## js-data-redis
+
+Redis adapter for [js-data](http://www.js-data.io/js-data).
+
+## API Documentation
+[DSRedisAdapter](https://github.com/js-data/js-data/wiki/DSRedisAdapter)
+
+## Project Status
+
+| Branch | Master |
+| ------ | ------ |
+| NPM | [![NPM version](https://badge.fury.io/js/js-data-redis.png)](http://badge.fury.io/js/js-data-redis) |
+| Build Status | [![Build Status](https://travis-ci.org/js-data/js-data-redis.png?branch=master)](https://travis-ci.org/js-data/js-data-redis) |
+| Code Climate | [![Code Climate](https://codeclimate.com/github/js-data/js-data-redis.png)](https://codeclimate.com/github/js-data/js-data-redis) |
+| Dependency Status | [![Dependency Status](https://gemnasium.com/js-data/js-data-redis.png)](https://gemnasium.com/js-data/js-data-redis) |
+| Coverage | [![Coverage Status](https://coveralls.io/repos/js-data/js-data-redis/badge.png?branch=master)](https://coveralls.io/r/js-data/js-data-redis?branch=master) |
+
+## Quick Start
+`npm install --save js-data js-data-redis`.
+
+```js
+var JSData = require('js-data');
+var DSRedisAdapter = require('js-data-redis');
+
+var store = new JSData.DS();
+
+var adapter = new DSRedisAdapter();
+
+store.registerAdapter('redis', adapter, { default: true });
+
+// "store" will now use the Redis adapter for all async operations
+```
+
+## Changelog
+[CHANGELOG.md](https://github.com/js-data/js-data-redis/blob/master/CHANGELOG.md)
+
+## Community
+- [Mailing List](https://groups.io/org/groupsio/jsdata) - Ask your questions!
+- [Issues](https://github.com/js-data/js-data-redis/issues) - Found a bug? Feature request? Submit an issue!
+- [GitHub](https://github.com/js-data/js-data-redis) - View the source code for js-data.
+- [Contributing Guide](https://github.com/js-data/js-data-redis/blob/master/CONTRIBUTING.md)
+
+## Contributing
+
+First, feel free to contact me with questions. [Mailing List](https://groups.io/org/groupsio/jsdata). [Issues](https://github.com/js-data/js-data-redis/issues).
+
+1. Contribute to the issue that is the reason you'll be developing in the first place
+1. Fork js-data-redis
+1. `git clone https://github.com/<you>/js-data-redis.git`
+1. `cd js-data-redis; npm install; bower install;`
+1. `grunt go` (builds and starts a watch)
+1. (in another terminal) `grunt karma:dev` (runs the tests)
+1. Write your code, including relevant documentation and tests
+1. Submit a PR and we'll review
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Jason Dobry
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ src/index.js
@@ -0,0 +1,175 @@
+var JSData = require('js-data');
+var redis = require('redis');
+var P = require('es6-promise').Promise;
+var forOwn = require('mout/object/forOwn');
+var keys = require('mout/object/keys');
+var deepMixIn = require('mout/object/deepMixIn');
+var forEach = require('mout/array/forEach');
+var contains = require('mout/array/contains');
+var isObject = require('mout/lang/isObject');
+var isString = require('mout/lang/isString');
+var upperCase = require('mout/string/upperCase');
+var underscore = require('mout/string/underscore');
+var guid = require('mout/random/guid');
+var emptyStore = new JSData.DS();
+var filter = emptyStore.defaults.defaultFilter;
+
+function getPath(resourceConfig) {
+  if (resourceConfig) {
+    return resourceConfig.table || underscore(resourceConfig.name);
+  }
+}
+
+function Defaults() {
+
+}
+
+function DSRedisAdapter(options) {
+  options = options || {};
+  this.defaults = new Defaults();
+  deepMixIn(this.defaults, options);
+  this.client = redis.createClient(this.defaults);
+}
+
+var dsRedisAdapterPrototype = DSRedisAdapter.prototype;
+
+dsRedisAdapterPrototype.getIds = function (resourceConfig) {
+  var _this = this;
+  return new P(function (resolve, reject) {
+    return _this.client.SMEMBERS(getPath(resourceConfig), function (err, ids) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(ids);
+      }
+    });
+  });
+};
+
+dsRedisAdapterPrototype.GET = function (path) {
+  var _this = this;
+  return new P(function (resolve, reject) {
+    return _this.client.GET(path, function (err, value) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(value));
+      }
+    });
+  });
+};
+
+dsRedisAdapterPrototype.find = function (resourceConfig, id) {
+  var _this = this;
+  return new P(function (resolve, reject) {
+    return _this.client.GET(getPath(resourceConfig) + '-' + id, function (err, item) {
+      if (err) {
+        reject(err);
+      } else if (!item) {
+        reject(new Error('Not Found!'));
+      } else {
+        resolve(JSON.parse(item));
+      }
+    });
+  });
+};
+
+dsRedisAdapterPrototype.findAll = function (resourceConfig, params) {
+  var _this = this;
+  return _this.getIds(resourceConfig).then(function (ids) {
+    var tasks = [];
+    var path = getPath(resourceConfig);
+
+    forEach(ids, function (id) {
+      tasks.push(_this.GET(path + '-' + id));
+    });
+
+    return P.all(tasks);
+  }).then(function (items) {
+    return filter.call(emptyStore, items, resourceConfig.name, params, { allowSimpleWhere: true });
+  });
+};
+
+dsRedisAdapterPrototype.create = function (resourceConfig, attrs) {
+  var _this = this;
+  return new P(function (resolve, reject) {
+    attrs[resourceConfig.idAttribute] = attrs[resourceConfig.idAttribute] || guid();
+    return _this.client
+      .multi()
+      .SET(getPath(resourceConfig) + '-' + attrs[resourceConfig.idAttribute], JSON.stringify(attrs))
+      .SADD(getPath(resourceConfig), attrs[resourceConfig.idAttribute])
+      .exec(function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(attrs);
+        }
+      });
+  });
+};
+
+dsRedisAdapterPrototype.update = function (resourceConfig, id, attrs) {
+  var _this = this;
+  return new P(function (resolve, reject) {
+    var path = getPath(resourceConfig) + '-' + id;
+    return _this.client.GET(path, function (err, value) {
+      if (err) {
+        reject(err);
+      } else if (!value) {
+        reject(new Error('Not Found!'));
+      } else {
+        value = JSON.parse(value);
+        deepMixIn(value, attrs);
+        _this.client.SET(path, JSON.stringify(value), function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(value);
+          }
+        });
+      }
+    });
+  });
+};
+
+dsRedisAdapterPrototype.updateAll = function (resourceConfig, attrs, params) {
+  var _this = this;
+  return _this.findAll(resourceConfig, params).then(function (items) {
+    var tasks = [];
+    forEach(items, function (item) {
+      tasks.push(_this.update(resourceConfig, item[resourceConfig.idAttribute], attrs));
+    });
+    return P.all(tasks);
+  });
+};
+
+dsRedisAdapterPrototype.destroy = function (resourceConfig, id) {
+  var _this = this;
+  return new P(function (resolve, reject) {
+    var path = getPath(resourceConfig);
+    return _this.client
+      .multi()
+      .DEL(path + '-' + id)
+      .SREM(path, id)
+      .exec(function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+  });
+};
+
+dsRedisAdapterPrototype.destroyAll = function (resourceConfig, params) {
+  var _this = this;
+  return _this.findAll(resourceConfig, params).then(function (items) {
+    var tasks = [];
+    forEach(items, function (item) {
+      tasks.push(_this.destroy(resourceConfig, item[resourceConfig.idAttribute]));
+    });
+    return P.all(tasks);
+  });
+};
+
+module.exports = DSRedisAdapter;
